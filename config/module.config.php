34187c1ccf5fe1d241ad3b7bce459f671a612f3b
Emanuele Minotto <minottoemanuele@gmail.com>
2015-02-08 21:15:24 +0100

    module configuration

--- config/module.config.php
+++ config/module.config.php
@@ -1,3 +1,18 @@
 <?php
 
-return array();
+return array(
+    // Embedly configuration
+    'embedly' => array(
+        'api_key' => null,
+        'http_client' => null,
+    ),
+    // Service Manager configuration
+    'service_manager' => array(
+        'aliases' => array(
+            'embedly' => 'EmbedlyModule\\Service\\Client',
+        ),
+        'factories' => array(
+            'EmbedlyModule\\Service\\Client' => 'EmbedlyModule\\Service\\ClientFactory',
+        ),
+    ),
+);
